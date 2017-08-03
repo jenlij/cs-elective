50dadd78889f28e28d65be4bc2c9d6fb7c2f46ca
jenlij <jljohns1216@gmail.com>
2017-08-03 18:23:09 -0400

    bubble sort complete

---
+++ day1/bubble_sort_ex.py
@@ -0,0 +1,44 @@
+# Implement the bubble sort algorithm in the bubble_sort method.
+# Return the sorted array.
+#
+# Helpful Resources
+# https://www.youtube.com/watch?v=8Kp-8OGwphY
+# http://interactivepython.org/runestone/static/pythonds/SortSearch/TheBubbleSort.html
+#
+
+def bubble_sort(my_array):
+    swapped = True
+    arrLength = len(my_array)
+    while swapped == True:
+        swapped = False
+        for i in range(arrLength-1):
+            if my_array[i] > my_array[i+1]:
+                temp1 = my_array[i]
+                temp2 = my_array[i+1]
+                my_array[i+1] = temp1
+                my_array[i] = temp2
+                swapped = True
+           
+    #repeat loop 1 until no two values were swapped
+    #repeat loop 2 comparing array[i] to array[i+1]
+    #if array[i]  > array[i+1], swap and set swapped = true
+    return my_array
+
+# print bubble_sort([5,4,3,2,1])
+
+
+
+input_arrays = [
+    [],
+    [9, 8, 7, 6, 5, 4, 3, 2, 1],
+    [1, 2, 3, 4],
+    [4, 6, 1, 3, 7, 8, 4, 3, 4],
+    [1],
+    [1, 3, 2]
+]
+
+for array in input_arrays:
+    print("")
+    print(" Input: " + str(array))
+    sorted_array = bubble_sort(array)
+    print("Output: " + str(sorted_array))
