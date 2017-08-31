5f3997d1d22283d7c18afd8f34b9f4c1944eee5c
jenlij <jljohns1216@gmail.com>
2017-08-31 17:58:25 -0400

    queue methods

--- day7/queue.py
+++ day7/queue.py
@@ -3,6 +3,8 @@ class Node:
         self.value = value
         self.previous = previous
 
+#first in first out. enqueue end, dequeue beginning
+
 
 class MyQueue:
     def __init__(self):
@@ -17,12 +19,25 @@ class MyQueue:
     def dequeue(self):
         #return next node and remove from queue
         node_to_remove = self.first
-        self.first = self.first.previous
+        if node_to_remove == self.last:
+            self.last = None
+            self.first = None
+        else:
+            self.first = self.first.previous
         return node_to_remove
 
     def enqueue(self, value):
         #add new node to end of queue
         new_node = Node(value)
-        self.first = new_node
-        return self
-
+        if self.first == None and self.last == None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.previous = new_node    
+            self.last = new_node
+        
+queue = MyQueue()
+queue.enqueue(1)
+queue.enqueue(5)
+print(queue.dequeue().value)
+print(queue.dequeue().value)
